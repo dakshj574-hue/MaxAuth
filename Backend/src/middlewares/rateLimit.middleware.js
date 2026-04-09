75328af2b7c81828fb7e3782f7a6548eb8ab4742
Daksh Jain <dakshj574@gmail.com>
2026-04-09 13:29:21 +0530

    Update rateLimit.middleware.js

--- Backend/src/middlewares/rateLimit.middleware.js
+++ Backend/src/middlewares/rateLimit.middleware.js
@@ -1,9 +1,13 @@
 import rateLimit from 'express-rate-limit';
 
 export const otpLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 3, // Limit each IP to 3 OTP requests 
-  message: { status: 'error', message: 'Too many OTP requests from this IP. Please try again after 10 minutes.' },
+  windowMs: 10 * 60 * 1000,
+  max: 3,
   standardHeaders: true,
   legacyHeaders: false,
+  message: {
+    success: false,
+    message: 'Too many OTP requests from this IP. Please try again after 10 minutes.',
+    data: {}
+  }
 });
